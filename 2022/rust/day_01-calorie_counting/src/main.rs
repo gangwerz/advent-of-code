41bee91e7e9b1b339e60ee9152ffe9f9bef0663d
Zach Gangwer <code@gangwerz.com>
2023-10-09 18:57:38 -0700

    2022-Rust-Day 01 - MVP Complete

---
+++ 2022/rust/day_01-calorie_counting/src/main.rs
@@ -0,0 +1,32 @@
+use std::{env, fs};
+
+fn main() {
+	let args: Vec<String> = env::args().collect();
+	let file = fs::read_to_string(&args[1]).expect("UNABLE TO READ FILE");
+
+	let mut elves: Vec<Vec<u64>> = Vec::new();
+	let mut elf: Vec<u64> = Vec::new();
+
+	for elem in file.lines() {
+		if elem == "" {
+			elves.push(elf.clone());
+
+			elf = Vec::new();
+		} else {
+			elf.push(elem.parse().unwrap());
+		}
+	}
+
+	let mut heft = 0;
+	let mut phattest = 0;
+	for (idx, e) in elves.iter().enumerate() {
+		let sum: u64 = e.iter().sum();
+
+		if sum > heft {
+			phattest = idx+1;
+			heft = sum;
+		}
+	}
+
+	println!("The most Caloric Elf is: {phattest}");
+}

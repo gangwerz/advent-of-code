798d96aed56f4c58eed0b14d85927a5b06cd1855
Zach Gangwer <code@gangwerz.com>
2023-10-10 18:46:55 -0700

    2022-Rust-Day 02 - Resync

--- 2022/rust/day_02-rock_paper_scissors/src/main.rs
+++ 2022/rust/day_02-rock_paper_scissors/src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use std::collections::HashMap;
+
+
+fn main() { 
+    let mut col_1 = HashMap::new();
+    let mut col_2 = HashMap::new();
+
+    col_1.insert(String::from("A"), String::from("Rock"));
+    col_1.insert(String::from("B"), String::from("Paper"));
+    col_1.insert(String::from("C"), String::from("Scissors"));
+
+    col_2.insert(String::from("X"), String::from("Rock"));
+    col_2.insert(String::from("Y"), String::from("Paper"));
+    col_2.insert(String::from("Z"), String::from("Scissors"));
 }
